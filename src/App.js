44bfd2bca4b4bcb6e413a2725249def67180fe67
JeongTaehwan <wjdxoghks11222@naver.com>
2021-08-23 15:19:44 +0900

    [ADD] axios와 styled-components 불러옴

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 function App() {
   return (
     <div>
-      Test
+
     </div>
   );
 }
---
+++ src/components/api.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+function api() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default api;
