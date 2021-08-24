03a557050078c89b07128ba439077ec9bb218d40
JeongTaehwan <wjdxoghks11222@naver.com>
2021-08-24 18:23:42 +0900

    [ADD] 코드 추가 및 오류 해결중

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import api from './components/Api';
 
 function App() {
   return (
     <div>
-
+      <api />
     </div>
   );
 }
--- src/components/api.js
+++ src/components/api.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-function api() {
+function Api() {
+    const [info, setInfo] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchInfo = async () => {
+            try {
+                setInfo(true);
+                setInfo(null);
+                const getInfo = await axios.get('https://www.scorebat.com/video-api/v3/');
+
+                fetchInfo(getInfo.data);
+            } catch (e) {
+                setError(e);
+            }
+            setInfo(null);
+        }
+        fetchInfo();
+    }, []);
+
+    if (!info) return null;
+    if (error) return <div>오류발생!</div>;
+
     return (
         <div>
-
+            <ul>
+                {info.map(info => (
+                    <li key={info.title}>
+                        {info.title} ({info.competition})
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
 
-export default api;
+export default Api;
 
 // https://geek-jokes.sameerkumar.website/api?format=json: api 주소
+// https://www.scorebat.com/video-api/v3/ : api 주소 2
