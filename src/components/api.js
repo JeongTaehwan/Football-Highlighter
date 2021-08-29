93a2295c4e1f54f0628543a218e1895737cbbd64
JeongTaehwan <wjdxoghks11222@naver.com>
2021-08-29 23:45:19 +0900

    [TEST] 오류 해결 전 test commit 1

--- src/components/api.js
+++ src/components/api.js
@@ -2,40 +2,45 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Api() {
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(null);
     const [info, setInfo] = useState(null);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         const fetchInfo = async () => {
             try {
-                setInfo(null);
-                setError(null);
+                setInfo(false);
+                setError(false);
+                console.log('info와 error값 false');
                 setLoading(true);
-                // 초깃값 설정을 해주고!
-                const getInfo = await axios.get('https://www.scorebat.com/video-api/v3/');
-                // api 불러오고!
-                fetchInfo(getInfo.data);
+                console.log('loading 값 true');
+                // 초깃값 설정
+
+                const getInfo = axios.get('https://www.scorebat.com/video-api/v3/'); // api 호출
+                fetchInfo(getInfo.data); // data를 받아옴
+                console.log('api 받아옴');
+                setInfo(true);
             } catch (e) {
+                setInfo(false);
                 setError(e);
+                console.log('에러발생');
             }
-            setLoading(false);
         }
         fetchInfo();
     }, []);
 
-    if (loading) return <div>로딩중입니다, 잠시만 기다려주세요...</div>;
-    if (error) return <div>오류가 발생하였습니다.</div>;
+    if (loading) return <div>로딩중...</div>
+    if (error) return <div>에러가 발생하였습니다.</div>
     if (!info) return null;
 
     return (
         <div>
-            <ul>
-                {info.response.map(info => (
-                    <li key={info.title}>
-                        {info.competition}
+            <ul key={info.title}>
+                {info.response.map(
+                    <li>
+                        {info.title} ({info.competition})
                     </li>
-                ))}
+                )}
             </ul>
         </div>
     );
