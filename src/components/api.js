d7b3cd7b9af9bc10ea5e5e652fca1d656b9cb659
JeongTaehwan <wjdxoghks11222@naver.com>
2021-08-30 19:57:40 +0900

    [TEST] 오류 해결 전 commit & useEffect 분리

--- src/components/api.js
+++ src/components/api.js
@@ -2,30 +2,30 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Api() {
-    const [loading, setLoading] = useState(null);
+    const [loading, setLoading] = useState(false);
     const [info, setInfo] = useState(null);
-    const [error, setError] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchInfo = async () => {
+        try {
+            setInfo(null);
+            setError(null);
+            console.log('info와 error값 false');
+            setLoading(true);
+            console.log('loading 값 true');
+            // 초깃값 설정
+
+            const response = axios.get('https://www.scorebat.com/video-api/v3/'); // api 호출
+            setInfo(response.data); // data를 받아옴
+            console.log('api 받아옴');
+        } catch (e) {
+            setError(e);
+        }
+        setLoading(false)
+    }
+    fetchInfo();
 
     useEffect(() => {
-        const fetchInfo = async () => {
-            try {
-                setInfo(false);
-                setError(false);
-                console.log('info와 error값 false');
-                setLoading(true);
-                console.log('loading 값 true');
-                // 초깃값 설정
-
-                const getInfo = axios.get('https://www.scorebat.com/video-api/v3/'); // api 호출
-                fetchInfo(getInfo.data); // data를 받아옴
-                console.log('api 받아옴');
-                setInfo(true);
-            } catch (e) {
-                setInfo(false);
-                setError(e);
-                console.log('에러발생');
-            }
-        }
         fetchInfo();
     }, []);
 
@@ -47,6 +47,5 @@ function Api() {
 }
 
 export default Api;
-
 // https://geek-jokes.sameerkumar.website/api?format=json: api 주소
 // https://www.scorebat.com/video-api/v3/ : api 주소 2
