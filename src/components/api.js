8961b9fa0b4dd71662e6af08a064bde8e2391d71
JeongTaehwan <wjdxoghks11222@naver.com>
2021-08-31 22:34:08 +0900

    [TEST] 오류 해결 전 test commit

--- src/components/api.js
+++ src/components/api.js
@@ -3,46 +3,41 @@ import axios from 'axios';
 
 function Api() {
     const [loading, setLoading] = useState(false);
-    const [info, setInfo] = useState(null);
+    const [infos, setInfo] = useState(null);
     const [error, setError] = useState(null);
 
-    const fetchInfo = async () => {
-        try {
-            setInfo(null);
-            setError(null);
-            console.log('info와 error값 false');
-            setLoading(true);
-            console.log('loading 값 true');
-            // 초깃값 설정
-
-            const response = axios.get('https://www.scorebat.com/video-api/v3/'); // api 호출
-            setInfo(response.data); // data를 받아옴
-            console.log('api 받아옴');
-        } catch (e) {
-            setError(e);
-        }
-        setLoading(false)
-    }
-    fetchInfo();
-
     useEffect(() => {
+        const fetchInfo = async () => {
+            try {
+                setInfo(null);
+                setError(null);
+                console.log('info와 error값 false');
+                setLoading(true);
+                console.log('loading 값 true');
+                // 초깃값 설정
+                const getInfo = await axios.get('https://www.scorebat.com/video-api/v3/'); // api 호출
+                setInfo(getInfo.data); // data를 받아옴
+                console.log('api 받아옴');
+            } catch (e) {
+                setError(e);
+            }
+            setLoading(false)
+        };
         fetchInfo();
     }, []);
 
     if (loading) return <div>로딩중...</div>
     if (error) return <div>에러가 발생하였습니다.</div>
-    if (!info) return null;
+    if (!infos) return null;
 
     return (
-        <div>
-            <ul key={info.title}>
-                {info.response.map(
-                    <li>
-                        {info.title} ({info.competition})
-                    </li>
-                )}
-            </ul>
-        </div>
+        <ul>
+            {infos.map(infos => (
+                <li key={infos.title}>
+                    {infos.title}
+                </li>
+            ))}
+        </ul>
     );
 }
 
